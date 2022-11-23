5afbd1664b2408c05bb551818821e27837bdb9f1
fardinr <fardinrajeit786@gmail.com>
2022-11-23 23:22:15 +0530

    minor changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 app = flask.Flask(__name__)
 
-@app.route('/',method = ['GET'])
+@app.route('/',methods = ['GET'])
 def home():
   arg=request.args['arg1']
   return 'hello world'
